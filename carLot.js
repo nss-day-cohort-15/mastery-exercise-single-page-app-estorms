485f4c86c4e590ca02a22298a5e37351fdde9090
Elizabeth Sanger <libsang@gmail.com>
2016-07-27 18:30:07 -0500

    baby steps

---
+++ carLot.js
@@ -0,0 +1,17 @@
+var CarLot = (function () {
+  var inventory = [];
+  inventory.push[];
+  return {
+    getInventory: function () {
+      return inventory;
+    },
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+
+      });
+    }
+  };
+
+})();
