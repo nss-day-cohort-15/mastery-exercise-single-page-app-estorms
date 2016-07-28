d750b4136ac91b78a5bd8d655dfd79d74617e412
Elizabeth Sanger <libsang@gmail.com>
2016-07-27 19:18:50 -0500

    CarLot IIFE complete

--- carLot.js
+++ carLot.js
@@ -1,17 +1,21 @@
 var CarLot = (function () {
   var inventory = [];
-  inventory.push[];
-  return {
+
+    return {
+
     getInventory: function () {
       return inventory;
     },
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
 
-      inventoryLoader.addEventListener("load", function () {
-
-      });
+    loadInventory: function (functionToBeInvoked) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'inventory.json');
+      xhr.addEventListener('load', function(event) {
+      inventory = JSON.parse(event.target.responseText)
+        functionToBeInvoked(inventory)
+      })
+      xhr.send()
     }
-  };
+}
+})()
 
-})();
--- quiz.js
+++ quiz.js
@@ -1,10 +1,20 @@
-function populatePage (inventory) {
-  // Loop over the inventory and populate the page
-
-  // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
+function displayInventory(inventory) {
+    console.log(inventory)
 }
 
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-CarLot.loadInventory();
+CarLot.loadInventory(displayInventory)
+
+
+// function populatePage (inventory) {
+//   // Loop over the inventory and populate the page
+
+//   // Now that the DOM is loaded, establish all the event listeners needed
+//   CarLot.activateEvents();
+// }
+
+// // Load the inventory and send a callback function to be
+// // invoked after the process is complete
+// CarLot.loadInventory();
+
+
+console.log('connected?')
