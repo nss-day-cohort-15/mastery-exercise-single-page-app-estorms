39b1af16845f1f2d558dadcdad4e0e0070473bd6
Elizabeth Sanger <libsang@gmail.com>
2016-07-29 14:47:22 -0500

    Centered car cards on page

--- quiz.js
+++ quiz.js
@@ -5,9 +5,9 @@ function displayInventory(inventory) {
 CarLot.loadInventory(displayInventory)
 
 function populatePage (inventory) {
-  var carsContainer = document.getElementById('carsContainer')
+  var carsContainer = document.getElementById('containerRow')
   inventory.forEach(function (car) {
-    carsContainer.innerHTML += `<div><span>${car.make}</span><span> ${car.model}</span></div><div>${car.year}</div><div>&#36;${car.price}</div><div>${car.color}</div><div>${car.description}</div>`
+    containerRow.innerHTML += `<div class="col-md-4" id="carColumn"><span>${car.make}</span><span> ${car.model}</span><div>${car.year}<div>&#36;${car.price}<div>${car.color}<div>${car.description}</div></div></div></div></div>`
   })
 }
 
