d355f0fab220165a758d1885f25cb659a665af17
Elizabeth Sanger <libsang@gmail.com>
2016-07-29 15:48:32 -0500

    basic styling complete

--- quiz.js
+++ quiz.js
@@ -7,7 +7,7 @@ CarLot.loadInventory(displayInventory)
 function populatePage (inventory) {
   var carsContainer = document.getElementById('containerRow')
   inventory.forEach(function (car) {
-    containerRow.innerHTML += `<div class="col-md-4" id="carColumn"><span>${car.make}</span><span> ${car.model}</span><div>${car.year}<div>&#36;${car.price}<div>${car.color}<div>${car.description}</div></div></div></div></div>`
+    containerRow.innerHTML += `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="carColumn"><span style="font-size: 1.5em">${car.make}</span><span style="font-size: 1.5em">${car.model}</span><div>${car.year}<div>&#36;${car.price}<div>${car.color}<div>${car.description}</div></div></div></div></div>`
   })
 }
 
