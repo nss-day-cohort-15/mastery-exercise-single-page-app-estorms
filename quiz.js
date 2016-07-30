061836b968a702f79417e5e18a87ae3698da2bfa
Elizabeth Sanger <libsang@gmail.com>
2016-07-30 18:00:42 -0500

    have input field doing something ... not what I want it to, but something

--- quiz.js
+++ quiz.js
@@ -9,14 +9,14 @@ function populatePage (inventory) {
   var carsContainer = document.getElementById('containerRow')
   inventory.forEach(function (car) {
     containerRow.innerHTML +=
-    `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="allCars">
+    `<p><div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="allCars">
             <span style="font-size: 1.5em" class="carColumn">${car.make}</span>
             <span class="carColumn" style="font-size: 1.5em">${car.model}</span>
             <div class="carColumn">${car.year}
             <div class="carColumn">&#36;${car.price}
             <div class="carColumn">${car.color}
             <div class="carColumn" id="carDescription">${car.description}
-            </div></div></div></div></div>`
+            </div></div></div></div></div></p>`
   })
 }
 CarLot.loadInventory(populatePage);
@@ -26,10 +26,11 @@ function activateEvents () {
   console.log(event);
   console.log(event.target.parentElement)
   if (event.target.className === "carColumn") {
+    // document.getElementById('allCars').style.color = "red"
     event.target.parentElement.innerHTML="<p>FuckThis</p>"
 
 
-  // event.target.parentElement.remove();
+
 
 
   }
@@ -37,6 +38,23 @@ function activateEvents () {
 
 
 }
+
+function activateEvents(inventory) {
+    document.querySelector("body").addEventListener('click', function(event){
+        if (event.target.id === "button") {
+            for (var i = 0; i < inventory.length; i++) {
+           document.getElementById('inputField').value = inventory[i].description
+            // for ( var i = 0; i < inventory.length; i++) {
+            //     inventory[i].description = inputField.value
+            // }
+}
+        }
+    // for (var i = 0; i < inventory.length; i++) {
+    //     console.log(inventory[i].description)
+    // }
+}
+)}
+
 
 CarLot.loadInventory(activateEvents)
 
