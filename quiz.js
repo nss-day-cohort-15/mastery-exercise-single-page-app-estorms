331e4941761920ec247c332c21e70b64596a2913
Elizabeth Sanger <libsang@gmail.com>
2016-07-30 16:13:39 -0500

    struggling to get border function changing, need a commit anyway

--- quiz.js
+++ quiz.js
@@ -4,33 +4,48 @@ function displayInventory(inventory) {
 
 CarLot.loadInventory(displayInventory)
 
+
 function populatePage (inventory) {
   var carsContainer = document.getElementById('containerRow')
   inventory.forEach(function (car) {
-    containerRow.innerHTML += `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" class="carColumn" id="allCars"><span style="font-size: 1.5em" class="carColumn">${car.make}</span><span class="carColumn" style="font-size: 1.5em">${car.model}</span><div class="carColumn">${car.year}<div class="carColumn">&#36;${car.price}<div class="carColumn">${car.color}<div class="carColumn">${car.description}</div></div></div></div></div>`
+    containerRow.innerHTML +=
+    `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="allCars">
+            <span style="font-size: 1.5em" class="carColumn">${car.make}</span>
+            <span class="carColumn" style="font-size: 1.5em">${car.model}</span>
+            <div class="carColumn">${car.year}
+            <div class="carColumn">&#36;${car.price}
+            <div class="carColumn">${car.color}
+            <div class="carColumn" id="carDescription">${car.description}
+            </div></div></div></div></div>`
   })
 }
-
 CarLot.loadInventory(populatePage);
 
-// function activateEvents(inventory) {
-//     document.querySelector("body").addEventListener("click", function(event) {
-//         if (event.target.id === "carColumn") {
-//   console.log(event)
 
 function activateEvents(inventory) {
 document.querySelector("body").addEventListener("click",
 function(event) {
 if (event.target.className === "carColumn") {
-    console.log('you clicked on a car. You are a magical unicorn.')
-    // var carColumn = document.getElementById("carColumn");
-    // carColumn.style.color = red;
 
-}
 
-}
 
-    )
+    console.log(event.target)
+    console.log(event.target.parentElement)
+
+    console.log('fuck you')
+
+
+
+
+
+        document.getElementById('allCars').style.border = "10px solid black"
+
+        }
+
+
+    })
+
+
 
 
 
