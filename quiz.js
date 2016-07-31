c8097b6d8e83612ff49281c3571b7c4829f602f6
Elizabeth Sanger <libsang@gmail.com>
2016-07-31 15:48:29 -0500

    input field listener appending text to car body

--- quiz.js
+++ quiz.js
@@ -5,19 +5,18 @@ function displayInventory(inventory) {
 CarLot.loadInventory(displayInventory)
 
 
-function populatePage (inventory) {
-  var carsContainer = document.getElementById('containerRow')
-  var purchaseStatus
-  inventory.forEach(function (car) {
-    if (car.purchased === true) {
-        purchaseStatus = "Available!"
-    }
-    else {
-        purchaseStatus ="Sold out!"
-    }
+function populatePage(inventory) {
+    var carsContainer = document.getElementById('containerRow')
+    var purchaseStatus
+    inventory.forEach(function(car) {
+        if (car.purchased === true) {
+            purchaseStatus = "Available!"
+        } else {
+            purchaseStatus = "Sold out!"
+        }
 
-    containerRow.innerHTML +=
-    `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="allCars">
+        containerRow.innerHTML +=
+            `<div class="col-md-3" style= "border: 5px solid ${car.color}; box-shadow: 10px 5px 7px black" id="allCars">
             <h3 class="carColumn">${car.make} ${car.model}</h3>
             <p class="carColumn">${car.year}
             <p class="carColumn">&#36;${car.price}
@@ -25,43 +24,53 @@ function populatePage (inventory) {
             <p class="carColumn" id="carDescription">${car.description}
             <p class="carColumn" style = "font-weight: bold">${purchaseStatus}
             </p></p></p></p></p></div>`
-  })
+    })
 }
 CarLot.loadInventory(populatePage);
 
-function activateEvents (inventory) {
+function activateEvents(inventory) {
     var carSelected
     var inputField = document.getElementById('inputField')
 
 
 
-        document.querySelector('body').addEventListener('click', function (event){
-            if (event.target.className === "carColumn"){
-                carSelected = event.target.parentElement
-                carSelected.style.backgroundColor = "beige"
-                carSelected.style.borderWidth = "10px"
-                inputField.value = ''
-                inputField.focus()
+    document.querySelector('body').addEventListener('click', function(event) {
+        if (event.target.className === "carColumn") {
+            carSelected = event.target.parentElement
+            carSelected.style.backgroundColor = "beige"
+            carSelected.style.borderWidth = "10px"
+            inputField.value = ''
+            inputField.focus()
 
-            }
-
-
-                })
-
-        document.querySelector('body').addEventListener('keypress', function(event){
-
-            if (event.target.id === 'inputField' && event.keyCode === 13){
-                console.log('listener added to input field')
-                activeElement = document.activeElement
-                console.log(activeElement)
-                carDescription = document.getElementById('carDescription')
-                carDescription.innerText += inputField.value;
-
-            }
+            inputField.addEventListener('keypress', function(event) {
+                if (event.keyCode === 13) {
+                    console.log('listener added successfully')
+                    carSelected.innerHTML += inputField.value;
 
 
+                }
 
-        })
+            })
+
+        }
+
+
+    })
+
+    // document.querySelector('body').addEventListener('keypress', function(event){
+
+    //     if (event.target.id === 'inputField' && event.keyCode === 13){
+    //         console.log('listener added to input field')
+    //         activeElement = document.activeElement
+    //         console.log(activeElement)
+    //         carDescription = document.getElementById('carDescription')
+    //         carDescription.innerText += inputField.value;
+
+    //     }
+
+
+
+    // })
 
 
 
@@ -71,9 +80,3 @@ function activateEvents (inventory) {
 }
 
 CarLot.loadInventory(activateEvents)
-
-
-
-
-
-
