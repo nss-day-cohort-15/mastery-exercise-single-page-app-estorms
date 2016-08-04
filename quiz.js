c2d47d4c07c75800bbe7093dbb6f99918abae959
Elizabeth Sanger <libsang@gmail.com>
2016-08-04 11:49:10 -0500

    bower components removed?

--- quiz.js
+++ quiz.js
@@ -22,6 +22,7 @@
     })
 }
 CarLot.loadInventory(populatePage);
+// CarLot.getInventory(console.log)
 
   // Now that the DOM is loaded, establish all the event listeners needed
 
@@ -37,14 +38,14 @@ CarLot.loadInventory(populatePage);
             inputField.value = '' //clear input field
             inputField.focus() //bring cursor to input field
             inputField.addEventListener('keypress', function(event) { //add keypress event listener to input field
-                 if (event.keyCode === 13) {  //when user clicks enter, input field content appended to each car object
+                 // if (event.keyCode === 13) {  //when user clicks enter, input field content appended to each car object
                     console.log('listener added successfully')
                     carSelected.innerHTML += inputField.value;
                     inputField.value = '' //clear input field once value appended to car
 
                     carSelected.style.backgroundColor = "white" //change background back to original
                     carSelected.style.borderWidth = "5px" //change background back to original
-                }
+                // }
 
             })
             }
@@ -52,18 +53,17 @@ CarLot.loadInventory(populatePage);
     document.querySelector('#button').addEventListener('click', function (event){
         alert("I'm just a button. That's all I know")
     })
-    document.querySelector('body').addEventListener('click', function(event){
-        if (event.target.id === 'allCars'){
-            carSelected = event.srcElement
-            carSelected.style.backgroundColor = "beige" //change background of target div
-            carSelected.style.borderWidth = "10px" //change border width of target div
-            inputField.value = '' //clear input field
-            inputField.focus() //bring cursor to input field
-            carSelected.style.backgroundColor = "white" //change background back to original
-            carSelected.style.borderWidth = "5px" //change border back to original
+     document.querySelector('body').addEventListener('click', function(event){
+         if (event.target.id === 'allCars'){
+         console.log('allCars selected')
+         carSelected = event.srcElement
+         console.log(carSelected)
+         carSelected.style.backgroundColor = "beige" //change background of target div
+             carSelected.style.borderWidth = "10px" //change border width of target div
+             inputField.value = '' //clear input field
+             inputField.focus() //bring cursor to input field
+     }
 
-        }
-
-    })
-}
-CarLot.loadInventory(activateEvents)
+     })
+  }
+  CarLot.loadInventory(activateEvents)
