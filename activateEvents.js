e61d1a66c9280fc1fc28bd620e66e6cccbd8b507
Elizabeth Sanger <libsang@gmail.com>
2016-08-05 10:08:24 -0500

    input field value appends to car card on 'enter'

--- activateEvents.js
+++ activateEvents.js
@@ -13,14 +13,14 @@ var Carlot = (function (carlot) {
             inputField.value = '' //clear input field
             inputField.focus() //bring cursor to input field
             inputField.addEventListener('keypress', function(event) { //add keypress event listener to input field
-                 // if (event.keyCode === 13) {  //when user clicks enter, input field content appended to each car object
+                 if (event.keyCode === 13) {  //when user clicks enter, input field content appended to each car object
                     console.log('listener added successfully')
                     carSelected.innerHTML += inputField.value;
                     inputField.value = '' //clear input field once value appended to car
 
                     carSelected.style.backgroundColor = "white" //change background back to original
                     carSelected.style.borderWidth = "5px" //change background back to original
-                // }
+                }
 
             })
             }
@@ -32,13 +32,12 @@ var Carlot = (function (carlot) {
 
     document.querySelector('body').addEventListener('click', function(event){
          if (event.target.id === 'allCars'){
-         console.log('allCars selected')
-         carSelected = event.srcElement
-         console.log(carSelected)
-         carSelected.style.backgroundColor = "beige" //change background of target div
-             carSelected.style.borderWidth = "10px" //change border width of target div
-             inputField.value = '' //clear input field
-             inputField.focus() //bring cursor to input field
+        carSelected = event.srcElement
+        carSelected.style.backgroundColor = "beige" //change
+        // background of target div
+        carSelected.style.borderWidth = "10px" //change border width of target div
+        inputField.value = '' //clear input field
+        inputField.focus() //bring cursor to input field
      }
 
      })
